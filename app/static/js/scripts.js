dbdfd0bd2bd6bfb1fc0a8691a79dbfac1665bfd9
FerPonce42 <angel.pacsi@ucsp.edu.pe>
2024-10-08 16:29:11 -0500

    Maricielo carrusel 2.0

--- app/static/js/scripts.js
+++ app/static/js/scripts.js
@@ -1,73 +1,74 @@
+
 // Espera a que el DOM esté completamente cargado
 document.addEventListener('DOMContentLoaded', function() {
-    // Menú desplegable
-    const dropdowns = document.querySelectorAll('.dropdown');
+// Menú desplegable
+const dropdowns = document.querySelectorAll('.dropdown');
 
-    dropdowns.forEach(dropdown => {
-        dropdown.addEventListener('mouseover', function() {
-            this.querySelector('.dropdown-content').style.display = 'block';
-        });
-
-        dropdown.addEventListener('mouseout', function() {
-            this.querySelector('.dropdown-content').style.display = 'none';
-        });
+dropdowns.forEach(dropdown => {
+    dropdown.addEventListener('mouseover', function() {
+        this.querySelector('.dropdown-content').style.display = 'block';
     });
 
-    // Carrusel de imágenes
-    let currentIndex = 0;
-    const slides = document.querySelectorAll('.carousel img');
-    const indicators = document.querySelectorAll('.carousel-indicators button');
+    dropdown.addEventListener('mouseout', function() {
+        this.querySelector('.dropdown-content').style.display = 'none';
+    });
+});
 
-    function showSlide(index) {
-        const carouselWidth = document.querySelector('.carousel').clientWidth;
+// Carrusel de imágenes
+let currentIndex = 0;
+const slides = document.querySelectorAll('.carousel img');
+const indicators = document.querySelectorAll('.carousel-indicators button');
 
-        slides.forEach((slide, i) => {
-            slide.style.transform = `translateX(${(i - index) * carouselWidth}px)`;
-        });
+function showSlide(index) {
+    const carouselWidth = document.querySelector('.carousel').clientWidth;
 
-        indicators.forEach((indicator, i) => {
-            indicator.classList.toggle('active', i === index);
-        });
-
-        currentIndex = index;
-    }
-
-    indicators.forEach((button, index) => {
-        button.addEventListener('click', () => showSlide(index));
+    slides.forEach((slide, i) => {
+        slide.style.transform = `translateX(${(i - index) * carouselWidth}px)`;
     });
 
-    showSlide(0);
-
-    setInterval(function() {
-        let nextIndex = (currentIndex + 1) % slides.length;
-        showSlide(nextIndex);
-    }, 5000);
-
-    function alternarSeccion(seccionId) {
-        var seccion = document.getElementById(seccionId);
-        if (seccion.classList.contains('visible')) {
-            seccion.classList.remove('visible');
-            seccion.classList.add('oculto');
-        } else {
-            ocultarTodasLasSecciones(); 
-            seccion.classList.remove('oculto');
-            seccion.classList.add('visible');
-        }
-    }
-
-    function ocultarTodasLasSecciones() {
-        var secciones = document.querySelectorAll('.rectangulo');
-        secciones.forEach(function(seccion) {
-            seccion.classList.remove('visible');
-            seccion.classList.add('oculto');
-        });
-    }
-
-    document.querySelectorAll('.barra-opciones a').forEach(function(link) {
-        link.addEventListener('click', function(e) {
-            e.preventDefault();
-            var target = this.getAttribute('data-target');
-            alternarSeccion(target); 
-        });
+    indicators.forEach((indicator, i) => {
+        indicator.classList.toggle('active', i === index);
     });
+
+    currentIndex = index;
+}
+
+indicators.forEach((button, index) => {
+    button.addEventListener('click', () => showSlide(index));
+});
+
+showSlide(0);
+
+setInterval(function() {
+    let nextIndex = (currentIndex + 1) % slides.length;
+    showSlide(nextIndex);
+}, 5000);
+
+function alternarSeccion(seccionId) {
+    var seccion = document.getElementById(seccionId);
+    if (seccion.classList.contains('visible')) {
+        seccion.classList.remove('visible');
+        seccion.classList.add('oculto');
+    } else {
+        ocultarTodasLasSecciones(); 
+        seccion.classList.remove('oculto');
+        seccion.classList.add('visible');
+    }
+}
+
+function ocultarTodasLasSecciones() {
+    var secciones = document.querySelectorAll('.rectangulo');
+    secciones.forEach(function(seccion) {
+        seccion.classList.remove('visible');
+        seccion.classList.add('oculto');
+    });
+}
+
+document.querySelectorAll('.barra-opciones a').forEach(function(link) {
+    link.addEventListener('click', function(e) {
+        e.preventDefault();
+        var target = this.getAttribute('data-target');
+        alternarSeccion(target); 
+    });
+});
 });
